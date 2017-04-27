7a74952e94eb1da2f53ca3df404fb990e54f7af1
jpollak <jozsef.pollak@emarsys.com>
2017-04-27 11:13:40 +0200

    feat(sample-app): log core and engage versions on SampleApplication::onCreate

--- app/src/main/java/com/emarsys/mobileengage/sample/SampleApplication.java
+++ app/src/main/java/com/emarsys/mobileengage/sample/SampleApplication.java
@@ -1,6 +1,7 @@
 package com.emarsys.mobileengage.sample;
 
 import android.app.Application;
+import android.util.Log;
 
 import com.emarsys.mobileengage.MobileEngage;
 import com.emarsys.mobileengage.MobileEngageConfig;
@@ -13,6 +14,8 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.i(TAG, "Core version: " + com.emarsys.core.BuildConfig.VERSION_NAME);
+        Log.i(TAG, "MobileEngage version: " + com.emarsys.mobileengage.BuildConfig.VERSION_NAME);
         MobileEngageConfig config = new MobileEngageConfig.Builder()
                 .credentials("14C19-A121F", "PaNkfOD90AVpYimMBuZopCpm8OWCrREu")
                 .build();
