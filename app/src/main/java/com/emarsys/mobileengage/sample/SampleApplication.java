4e80266387597222edf043e2b2036445e9cd7d20
jpollak <jozsef.pollak@emarsys.com>
2017-06-15 15:41:40 +0200

    feat(application): make changes due to api change in mobile engage sdk

--- app/src/main/java/com/emarsys/mobileengage/sample/SampleApplication.java
+++ app/src/main/java/com/emarsys/mobileengage/sample/SampleApplication.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import com.emarsys.mobileengage.MobileEngage;
 import com.emarsys.mobileengage.MobileEngageConfig;
 
-
 public class SampleApplication extends Application {
 
     private static final String TAG = "SampleApplication";
@@ -17,8 +16,9 @@ public class SampleApplication extends Application {
         Log.i(TAG, "Core version: " + com.emarsys.core.BuildConfig.VERSION_NAME);
         Log.i(TAG, "MobileEngage version: " + com.emarsys.mobileengage.BuildConfig.VERSION_NAME);
         MobileEngageConfig config = new MobileEngageConfig.Builder()
+                .application(this)
                 .credentials("14C19-A121F", "PaNkfOD90AVpYimMBuZopCpm8OWCrREu")
                 .build();
-        MobileEngage.setup(this, config);
+        MobileEngage.setup(config);
     }
 }
